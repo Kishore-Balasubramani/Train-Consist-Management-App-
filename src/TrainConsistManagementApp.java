a8a84189c0679a3bdc41bd271ee589b2bf733248
Kishore-Balasubramani <kishorebalasubramani.2006@gmail.com>
2026-03-25 09:37:46 +0530

    UC1 - Initialize Train and Display Consist

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,3 +1,14 @@
-public class TrainConsistManagementApp {
+import java.util.ArrayList;
+import java.util.List;
 
+public class TrainConsistManagementApp {
+    public static void main(String[] args){
+        System.out.println("=================================\n");
+        System.out.println("Train Consist Management App \n");
+        System.out.println("=================================\n");
+        List<String> trainConsist = new ArrayList<>();
+        System.out.println("Train initialized Successfully....");
+
+    }
 }
+
